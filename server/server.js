c80edaee52a735d46a66e400a126b119276c6929
LakshyaGupta2782 <guptalakshya368@gmail.com>
2025-10-06 15:51:57 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ import 'dotenv/config'
 import  cors from 'cors'
 import connectDB from './configs/db.js'
 import adminRouter from './routes/adminRoutes.js';
-
+import blogRouter from './routes/blogRoutes.js';
 
 const app = express();
 
 await connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
 
 app.listen(PORT, () => {
